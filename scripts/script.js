e41c72730b4624054ed377a837b14cb9a505eeab
Vadim <nukedice@gmail.com>
2023-08-10 11:29:33 +0500

    6 cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll('.slider__image')
+function initSlider () {const images = document.querySelectorAll('.slider__image')
 const dots = document.querySelectorAll('.slider__dot')
 dots.forEach((dot, index) => {
     dot.addEventListener('click', () => {
@@ -26,3 +26,60 @@ function showNextSlide() {
     }
 }
 setInterval(showNextSlide, 3000)
+}
+document.addEventListener('load', initSlider())
+    const API_KEY = 'AIzaSyDR2SMVcmo3MnKW4-4brdX7NoTDbRiKTZc';
+    let quantityCards = 6;
+    let category = 'Architecture';
+    const URL = `https://www.googleapis.com/books/v1/volumes?q="subject:${category}"&key=${API_KEY}&printType=books&startIndex=0&maxResults=${quantityCards}&langRestrict=en`;
+    const cards = document.querySelector('.book_cards')
+    const more = document.querySelector('.more')
+    const genreList = document.querySelectorAll('.list_item')
+    function showMoreCards() {
+        removeCards();
+        getCards();
+        return quantityCards += 6; //не изменяет ссылку + плохое решение
+    }
+    more.addEventListener('click',()=>{ showMoreCards(); getCards()})
+    function removeCards () {
+        while (cards.lastChild) {
+            cards.lastChild.remove()
+        }
+    }
+    function makeShorterDescription (str) {
+        if (str) {
+            return str.slice (0, 147) + '...'
+        }
+        else {
+            return 'there is no description for this book'
+        }
+        //str? str.slice(0, 200):'there is no description'
+        //str.length>200? str.slice(0, 197) + '...': str         не работает
+    }
+
+    function createCards(data) {
+        //removeCards();
+        cards.insertAdjacentHTML('beforeend', `<div class="card">
+        <img src="${data.volumeInfo.imageLinks.thumbnail}" class="image">
+        <div class="about">
+            <div class="author">${data.volumeInfo.authors?data.volumeInfo.authors:'неизвестен'}</div>
+            <div class="name">${data.volumeInfo.title}</div>
+            <div class="rate">
+                <div class="stars">****</div>
+                <div class="review">reviews</div>
+            </div>
+            <div class="discription">${makeShorterDescription(data.volumeInfo.description)}</div>
+            <div class="price">price</div>
+            <button class="buy_btn">button</button>
+        </div>
+    </div>`)
+    }
+    function getCards() {
+    fetch(URL).then(function(res) {
+        return res.json()
+    }).then(function(data) {
+        console.log(data.items)
+        data.items.map(card => createCards(card))
+    }).catch(function(err) { console.log(err)})
+}
+document.addEventListener('load', getCards())
